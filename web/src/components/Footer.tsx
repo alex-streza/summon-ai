5d54b4091fff78b6fa56776fba8ae3c2fda9beac
alex-streza <alex.streza@okapistudio.com>
2022-12-26 15:30:28 +0200

    add product hunt badge

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -10,6 +10,18 @@ const Footer = ({ className = "bg-white" }: FooterProps) => {
       className={`
         ${className} z-10 mt-auto flex w-full items-center justify-between border-t border-gray-900 py-5 md:py-12`}
     >
+      <a
+        className="hidden md:block"
+        href="https://www.producthunt.com/posts/summon-ai?utm_source=badge-top-post-topic-badge&utm_medium=badge&utm_souce=badge-summon&#0045;ai"
+        target="_blank"
+      >
+        <img
+          src="https://api.producthunt.com/widgets/embed-image/v1/top-post-topic-badge.svg?post_id=366943&theme=dark&period=weekly&topic=Artificial Intelligence"
+          alt="Summon&#0046;AI - Generate&#0032;beautiful&#0032;images&#0032;with&#0032;DALL&#0045;E&#0045;2&#0032;in&#0032;Figma | Product Hunt"
+          width="250"
+          height="54"
+        />
+      </a>
       <ul className="grid grid-cols-2 gap-5 text-gray-500 uppercase md:mx-auto md:flex md:gap-16">
         <li>
           <Link href="/showcase">Showcase</Link>
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -9,7 +9,19 @@ const Home: NextPage = () => {
   return (
     <Layout>
       <NextSeo title="Summon AI" />
-      <h1 className="mt-16 mb-12 text-4xl md:text-7xl">
+      <a
+        className="mt-16 mb-3 md:hidden"
+        href="https://www.producthunt.com/posts/summon-ai?utm_source=badge-top-post-topic-badge&utm_medium=badge&utm_souce=badge-summon&#0045;ai"
+        target="_blank"
+      >
+        <img
+          src="https://api.producthunt.com/widgets/embed-image/v1/top-post-topic-badge.svg?post_id=366943&theme=dark&period=weekly&topic=Artificial Intelligence"
+          alt="Summon&#0046;AI - Generate&#0032;beautiful&#0032;images&#0032;with&#0032;DALL&#0045;E&#0045;2&#0032;in&#0032;Figma | Product Hunt"
+          width="250"
+          height="54"
+        />
+      </a>
+      <h1 className="mb-12 text-4xl md:mt-12 md:text-7xl">
         <div>
           <span className="mr-3 font-serif text-5xl italic font-bold transition-colors duration-300 hover:text-green-500 md:mr-12 md:text-8xl">
             Evolving
